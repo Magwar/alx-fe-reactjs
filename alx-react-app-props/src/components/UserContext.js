73931cf92a916863a3f49a1defae8fe18779aec7
magwar <williammagua@gmail.com>
2025-07-20 18:48:06 +0300

    feat: Create UserContext to manage user data across the application

---
+++ alx-react-app-props/src/components/UserContext.js
@@ -0,0 +1,13 @@
+//The main use of this file is to create a UserContext
+// that can be used to provide user data throughout the application.
+// This allows components to access user information without passing
+// props down manually at every level.
+
+//UserContext.js exists to define a shared state container (context)
+// that can be provided at a high level (like in App.jsx) and consumed deeply
+// (like in UserDetails.jsx)—eliminating the need to pass data through intermediate
+// components like ProfilePage and UserInfo.
+
+React.createContext();
+
+export default React.createContext
