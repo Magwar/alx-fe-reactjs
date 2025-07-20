5bd9f0c32379d1138b67910db18ea4ce70d0339c
magwar <williammagua@gmail.com>
2025-07-20 15:50:11 +0300

    feat: Initialize new React app with Vite and create basic components
    - Added package.json with dependencies for React, Vite, and ESLint.
    - Created public assets including vite.svg and react.svg.
    - Developed main application structure with App.jsx, including components: WelcomeMessage, Header, MainContent, Footer, and UserProfile.
    - Styled application with App.css and index.css for global styles.
    - Configured Vite with vite.config.js for development and build processes.
    - Updated existing App.jsx to import components from the new structure.

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,15 @@
+function MainContent() {
+  return (
+    <main 
+    style={{
+      padding: "20px", 
+      fontFamily: "Arial, sans-serif", 
+     backgroundColor: "lightgray" , 
+     color: "black",
+     fontSize }}>
+      <p>I love to visit New York, Paris, and Tokyo.</p>
+    </main>
+  );
+}
+
+export default MainContent;
