418bd17b965889b4aa66d0c35573310ed176507d
magwar <williammagua@gmail.com>
2025-08-02 12:51:58 +0300

    refactor: replace custom GitHub axios instance with direct axios calls in fetchUserData function

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,8 +1,16 @@
-import github from "./github";
+import axios from "axios";
+
+const BASE_URL = "https://api.github.com";
+const TOKEN = import.meta.env.VITE_GITHUB_TOKEN;
 
 export async function fetchUserData(username) {
   try {
-    const response = await github.get(`/users/${username}`);
+    const response = await axios.get(`${BASE_URL}/users/${username}`, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`,
+      },
+    });
+
     return response.data;
   } catch (error) {
     throw new Error("User not found");
